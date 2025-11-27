9bddb1a709bdea1f1cd770a9d59dd6e5c63f9e16
Chloe Wildman <wildmanchloe@outlook.com>
2025-11-27 10:51:55 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Countdown from "./components/Countdown.jsx";
+
+function App() {
+  return (
+    <div>
+        <h1>Wedding Countdown 💍</h1>
+        <Countdown />
+    </div>
+  );
+}
+
+export default App;
+
